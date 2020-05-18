aa868b12878fbb90e5cdf8921d9b1b7dff7dcb1d
Kim Willemstein <kimwillemstein@live.nl>
2020-05-18 11:54:05 +0200

    updates

---
+++ src/components/ModalContentOne.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="modal-wrapper">
+    <p class="intro">
+      Lottie is a mobile library for Android and iOS that parses Adobe After
+      Effects animations exported as json and renders them natively on
+      mobile!<br />
+      The lottie animation below were made for learning purposes.
+    </p>
+    <div class="lottie-section  py-8 flex justify-center text-center mx-0">
+      <lottie-player
+        src="https://assets9.lottiefiles.com/packages/lf20_cAvr6o.json"
+        background="transparent"
+        speed="1"
+        style="width: 300px; height: 300px;"
+        loop
+        autoplay
+      ></lottie-player>
+    </div>
+    <div class=" py-8 flex justify-center text-center mx-0">
+      <lottie-player
+        src="https://assets4.lottiefiles.com/packages/lf20_BCALBc.json"
+        background="transparent"
+        speed="1"
+        style="width: 300px; height: 300px;"
+        loop
+        autoplay
+      ></lottie-player>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "app",
+  data: function() {
+    return {
+      animationJSON: "../assets/pinjump.json"
+    };
+  },
+  components: {}
+};
+</script>
+<style lang="scss">
+.modal-wrapper {
+  @apply p-8;
+  .intro {
+    max-width: 600px;
+    @apply pb-4;
+  }
+  .lottie-section {
+    background: rgba(#2c3e50, 0.15);
+  }
+}
+</style>
